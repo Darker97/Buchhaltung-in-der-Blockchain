3d841cfdc6c5b28ad2a6db248e0c040345f94962
Christian Baltzer <christian@baltzer.de>
2019-08-03 16:04:49 +0200

    Tests eingefügt
    Alle Tests Grün

---
+++ Test/Test_Blockchain.java
@@ -0,0 +1,63 @@
+package com.ugsbo.Buchhaltung;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import ugsbo.com.buchhaltung.Blockchain;
+
+public class Test_Blockchain {
+
+  public Blockchain Workingobjekt;
+  
+  @Before
+  public void setUp() throws Exception {
+    Workingobjekt = new Blockchain();
+  }
+
+  @Test
+  public void hinzufügen() {
+    int eingabe = 500;
+    int ergebnis;
+    
+    Workingobjekt.add(eingabe);
+    ergebnis = Workingobjekt.kontostand();
+    
+    assertEquals("eingabe und Ergebnis sind gleich", eingabe, ergebnis);
+  }
+  
+  @Test
+  public void hinzufügenNegativ() {
+    int eingabe = -500;
+    int ergebnis;
+    
+    Workingobjekt.add(eingabe);
+    ergebnis = Workingobjekt.kontostand();
+    
+    assertEquals("eingabe und Ergebnis sind gleich", eingabe, ergebnis);
+  }
+
+  @Test
+  public void hinzufügenIstNull() {
+    int eingabe = 0;
+    int ergebnis;
+    
+    Workingobjekt.add(eingabe);
+    ergebnis = Workingobjekt.kontostand();
+    
+    assertEquals("eingabe und Ergebnis sind gleich", eingabe, ergebnis);
+  }
+  
+  @Test
+  public void hinzufügenMehrAlsEinmal() {
+    int eingabe = 100;
+    int erwartet = 300;
+    int ergebnis;
+    
+    Workingobjekt.add(eingabe);
+    Workingobjekt.add(eingabe);
+    Workingobjekt.add(eingabe);
+    ergebnis = Workingobjekt.kontostand();
+    
+    assertEquals("eingabe und Ergebnis sind gleich", erwartet, ergebnis);
+  }
+}
